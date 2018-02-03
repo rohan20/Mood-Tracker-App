60f28c20797dc546e2f994733e3f3ad0cd1f9eec
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-04 00:38:57 +0530

    referenced views in the activity

--- app/src/main/java/com/example/rohantaneja/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.rohantaneja.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton ibMoodMessage;
+    ImageButton ibMoodHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ibMoodMessage = findViewById(R.id.ib_mood_message);
+        ibMoodHistory = findViewById(R.id.ib_mood_history);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ib_mood_message:
+                addMoodMessage();
+                break;
+
+            case R.id.ib_mood_history:
+
+                break;
+        }
+    }
+
+    private void addMoodMessage() {
     }
 }
