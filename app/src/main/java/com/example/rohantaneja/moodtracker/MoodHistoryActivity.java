94d5990197be9739a7e7d663a6826695828df61f
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-07 21:18:20 +0530

    settings dimensions of a view dynamically

--- app/src/main/java/com/example/rohantaneja/moodtracker/MoodHistoryActivity.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/MoodHistoryActivity.java
@@ -1,7 +1,13 @@
 package com.example.rohantaneja.moodtracker;
 
+import android.graphics.Point;
+import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MoodHistoryActivity extends BaseActivity {
 
@@ -9,5 +15,23 @@ public class MoodHistoryActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_history);
+
+        View view = findViewById(R.id.dummy_view);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                display.getRealSize(size);
+            }
+        } catch (NoSuchMethodError err) {
+            display.getSize(size);
+        }
+        int width = size.x;
+        int height = size.y;
+
+        //setting view's width and height dynamically
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(width / 4, height / 2);
+        view.setLayoutParams(layoutParams);
     }
 }
