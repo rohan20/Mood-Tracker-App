f8544aff6964d515014d2e8ca251e72df03c9808
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-09 19:23:55 +0530

    added model class Mood

---
+++ app/src/main/java/com/example/rohantaneja/moodtracker/Mood.java
@@ -0,0 +1,13 @@
+package com.example.rohantaneja.moodtracker;
+
+/**
+ * Created by rohantaneja on 09/02/18.
+ */
+
+public class Mood {
+
+    private String moodName;
+    private int moodId;
+    private int moodResId;
+
+}
