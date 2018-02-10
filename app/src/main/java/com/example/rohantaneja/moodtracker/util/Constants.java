6d208293ab06aedc9f65ef7aa4d5a93e7f256a2a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-10 23:03:08 +0530

    added constans for mood colours

--- app/src/main/java/com/example/rohantaneja/moodtracker/util/Constants.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/util/Constants.java
@@ -16,4 +16,10 @@ public class Constants {
     public static final int MOOD_IMAGE_HAPPY = R.drawable.smiley_happy;
     public static final int MOOD_IMAGE_SUPER_HAPPY = R.drawable.smiley_super_happy;
 
+    public static final int MOOD_COLOR_SAD = R.color.faded_red;
+    public static final int MOOD_COLOR_DISAPPOINTED = R.color.warm_grey;
+    public static final int MOOD_COLOR_NORMAL = R.color.cornflower_blue_65;
+    public static final int MOOD_COLOR_HAPPY = R.color.light_sage;
+    public static final int MOOD_COLOR_SUPER_HAPPY = R.color.banana_yellow;
+
 }
