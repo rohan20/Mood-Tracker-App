65486261645b86e84ad11633f338c950e41721ff
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-10 22:53:53 +0530

    added MoodHistory adapter for RecyclerView

---
+++ app/src/main/java/com/example/rohantaneja/moodtracker/MoodHistoryAdapter.java
@@ -0,0 +1,44 @@
+package com.example.rohantaneja.moodtracker;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by rohantaneja on 10/02/18.
+ */
+public class MoodHistoryAdapter extends RecyclerView.Adapter<MoodHistoryViewHolder> {
+    private final Context context;
+    private List<Mood> items;
+
+    public MoodHistoryAdapter(List<Mood> items, Context context) {
+        this.items = items;
+        this.context = context;
+    }
+
+    @Override
+    public MoodHistoryViewHolder onCreateViewHolder(ViewGroup parent,
+                                                    int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.mood_history_item, parent, false);
+        return new MoodHistoryViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(MoodHistoryViewHolder holder, int position) {
+        Mood item = items.get(position);
+        //TODO Fill in your logic for binding the view.
+    }
+
+    @Override
+    public int getItemCount() {
+        if (items == null) {
+            return 0;
+        }
+        return items.size();
+    }
+}
