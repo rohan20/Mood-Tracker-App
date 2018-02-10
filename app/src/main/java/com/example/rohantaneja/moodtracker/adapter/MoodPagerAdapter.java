b2050460738947a2a61513bba265542c11ac0d8e
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-10 23:01:06 +0530

    added Constants for mood images

--- app/src/main/java/com/example/rohantaneja/moodtracker/adapter/MoodPagerAdapter.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/adapter/MoodPagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 import com.example.rohantaneja.moodtracker.R;
 import com.example.rohantaneja.moodtracker.ui.MoodFragment;
+import com.example.rohantaneja.moodtracker.util.Constants;
 
 /**
  * Created by rohantaneja on 05/02/18.
@@ -25,27 +26,27 @@ public class MoodPagerAdapter extends FragmentPagerAdapter {
         switch (position) {
 
             case 0:
-                moodImageResId = R.drawable.smiley_sad;
+                moodImageResId = Constants.MOOD_IMAGE_SAD;
                 moodBackgroundId = R.color.faded_red;
                 break;
 
             case 1:
-                moodImageResId = R.drawable.smiley_disappointed;
+                moodImageResId = Constants.MOOD_IMAGE_DISAPPOINTED;
                 moodBackgroundId = R.color.warm_grey;
                 break;
 
             case 2:
-                moodImageResId = R.drawable.smiley_normal;
+                moodImageResId = Constants.MOOD_IMAGE_NORMAL;
                 moodBackgroundId = R.color.cornflower_blue_65;
                 break;
 
             case 3:
-                moodImageResId = R.drawable.smiley_happy;
+                moodImageResId = Constants.MOOD_IMAGE_HAPPY;
                 moodBackgroundId = R.color.light_sage;
                 break;
 
             default:
-                moodImageResId = R.drawable.smiley_super_happy;
+                moodImageResId = Constants.MOOD_IMAGE_SUPER_HAPPY;
                 moodBackgroundId = R.color.banana_yellow;
         }
 
@@ -54,6 +55,6 @@ public class MoodPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 5;
+        return Constants.NUMBER_OF_MOODS;
     }
 }
--- app/src/main/java/com/example/rohantaneja/moodtracker/util/Constants.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/util/Constants.java
@@ -1,8 +1,19 @@
 package com.example.rohantaneja.moodtracker.util;
 
+import com.example.rohantaneja.moodtracker.R;
+
 /**
  * Created by rohantaneja on 10/02/18.
  */
 
 public class Constants {
+
+    public static final int NUMBER_OF_MOODS = 5;
+
+    public static final int MOOD_IMAGE_SAD = R.drawable.smiley_sad;
+    public static final int MOOD_IMAGE_DISAPPOINTED = R.drawable.smiley_disappointed;
+    public static final int MOOD_IMAGE_NORMAL = R.drawable.smiley_normal;
+    public static final int MOOD_IMAGE_HAPPY = R.drawable.smiley_happy;
+    public static final int MOOD_IMAGE_SUPER_HAPPY = R.drawable.smiley_super_happy;
+
 }
