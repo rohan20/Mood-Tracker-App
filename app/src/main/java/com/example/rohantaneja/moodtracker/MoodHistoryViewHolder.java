f4d391c844412f41870135a7182e1c73c3310e8d
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-10 22:10:40 +0530

    added MoodHistoryViewHolder

---
+++ app/src/main/java/com/example/rohantaneja/moodtracker/MoodHistoryViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.rohantaneja.moodtracker;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by rohantaneja on 10/02/18.
+ */
+
+public class MoodHistoryViewHolder extends RecyclerView.ViewHolder {
+
+    public MoodHistoryViewHolder(View itemView) {
+        super(itemView);
+    }
+}
