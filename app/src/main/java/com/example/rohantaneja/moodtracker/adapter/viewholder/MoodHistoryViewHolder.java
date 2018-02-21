252225efbcf467cad5f9016ab051cd097045ca98
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-02-21 21:55:02 +0530

    removed redundant parent layout from ViewHolder

--- app/src/main/java/com/example/rohantaneja/moodtracker/adapter/viewholder/MoodHistoryViewHolder.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/adapter/viewholder/MoodHistoryViewHolder.java
@@ -14,16 +14,14 @@ import com.example.rohantaneja.moodtracker.model.Mood;
 public class MoodHistoryViewHolder extends RecyclerView.ViewHolder {
 
     private View moodBackgroundView;
-    private ConstraintLayout parentConstraintLayout;
 
     public MoodHistoryViewHolder(View itemView) {
         super(itemView);
-        parentConstraintLayout = itemView.findViewById(R.id.parent_constraint_layout);
         moodBackgroundView = itemView.findViewById(R.id.mood_background_view);
     }
 
     public void bindData(Mood mood, double parentHeight, double parentWidth) {
         //todo set layout parents of each item according to the parent's width and mood of the item
-        parentConstraintLayout.setLayoutParams(new ConstraintLayout.LayoutParams((int) parentWidth / 2, (int) parentHeight / 2));
+        moodBackgroundView.setLayoutParams(new ConstraintLayout.LayoutParams((int) parentWidth / 2, (int) parentHeight / 2));
     }
 }
