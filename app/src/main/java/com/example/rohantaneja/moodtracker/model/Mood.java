8956e0f9cfe5496fa077adfdad230e58aed4ce40
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-05 17:08:09 +0530

    modified Mood class to have only moodId & moodMessage

--- app/src/main/java/com/example/rohantaneja/moodtracker/model/Mood.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/model/Mood.java
@@ -7,53 +7,23 @@ package com.example.rohantaneja.moodtracker.model;
 public class Mood {
 
     private int moodId;
-    private String moodName;
-    private int moodResId;
-    private int moodColorId;
     private String moodMessage;
 
-    public Mood(int moodId, String moodName, int moodResId, int moodColorId, String moodMessage) {
+    public Mood(int moodId, String moodMessage) {
         this.moodId = moodId;
-        this.moodName = moodName;
-        this.moodResId = moodResId;
-        this.moodColorId = moodColorId;
         this.moodMessage = moodMessage;
-    }
-
-    public String getMoodName() {
-        return moodName;
     }
 
     public int getMoodId() {
         return moodId;
     }
 
-    public int getMoodResId() {
-        return moodResId;
-    }
-
-    public int getMoodColorId() {
-        return moodColorId;
-    }
-
     public String getMoodMessage() {
         return moodMessage;
     }
 
-    public void setMoodName(String moodName) {
-        this.moodName = moodName;
-    }
-
     public void setMoodId(int moodId) {
         this.moodId = moodId;
-    }
-
-    public void setMoodResId(int moodResId) {
-        this.moodResId = moodResId;
-    }
-
-    public void setMoodColorId(int moodColorId) {
-        this.moodColorId = moodColorId;
     }
 
     public void setMoodMessage(String moodMessage) {
