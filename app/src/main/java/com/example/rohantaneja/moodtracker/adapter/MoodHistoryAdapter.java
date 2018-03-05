f30d688c2f495e388df5a953ce1b4d2bc99d0918
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-05 17:47:43 +0530

    removed redundant variable assignments in MoodHistoryAdapter

--- app/src/main/java/com/example/rohantaneja/moodtracker/adapter/MoodHistoryAdapter.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/adapter/MoodHistoryAdapter.java
@@ -2,15 +2,13 @@ package com.example.rohantaneja.moodtracker.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.rohantaneja.moodtracker.model.Mood;
-import com.example.rohantaneja.moodtracker.adapter.viewholder.MoodHistoryViewHolder;
 import com.example.rohantaneja.moodtracker.R;
-import com.example.rohantaneja.moodtracker.ui.MoodHistoryActivity;
+import com.example.rohantaneja.moodtracker.adapter.viewholder.MoodHistoryViewHolder;
+import com.example.rohantaneja.moodtracker.model.Mood;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -45,14 +43,12 @@ public class MoodHistoryAdapter extends RecyclerView.Adapter<MoodHistoryViewHold
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.mood_history_item, parent, false);
 
-        MoodHistoryViewHolder viewHolder = new MoodHistoryViewHolder(v, context);
-        return viewHolder;
+        return new MoodHistoryViewHolder(v, context);
     }
 
     @Override
     public void onBindViewHolder(MoodHistoryViewHolder holder, int position) {
-        Mood moodItem = moodsList.get(position);
-        holder.bindData(moodItem, parentHeight, parentWidth);
+        holder.bindData(moodsList.get(position), parentHeight, parentWidth);
     }
 
     @Override
