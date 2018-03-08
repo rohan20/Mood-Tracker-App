ca089bb20c3c961bd7f741fe11c5183b513568ea
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-08 07:19:16 +0530

    added SharedPreferenceUtils class

---
+++ app/src/main/java/com/example/rohantaneja/moodtracker/util/SharedPreferenceUtils.java
@@ -0,0 +1,38 @@
+package com.example.rohantaneja.moodtracker.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by rohantaneja on 08/03/18.
+ */
+
+public class SharedPreferenceUtils {
+
+    private static SharedPreferenceUtils mSharedPreferenceUtils;
+    protected Context mContext;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mSharedPreferencesEditor;
+
+    private SharedPreferenceUtils(Context context) {
+        mContext = context;
+        mSharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+    }
+
+    /**
+     * Creates single instance of SharedPreferenceUtils
+     *
+     * @param context context of Activity or Service
+     * @return Returns instance of SharedPreferenceUtils
+     */
+    public static synchronized SharedPreferenceUtils getInstance(Context context) {
+
+        if (mSharedPreferenceUtils == null) {
+            mSharedPreferenceUtils = new SharedPreferenceUtils(context.getApplicationContext());
+        }
+        return mSharedPreferenceUtils;
+    }
+
+
+}
