f9720a30b4d9ebda72dba119585ab9a69fdcb03f
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-09-21 07:28:19 +0530

    added method to initialise AlarmManager

--- app/src/main/java/com/example/rohantaneja/moodtracker/ui/MoodHistoryActivity.java
+++ app/src/main/java/com/example/rohantaneja/moodtracker/ui/MoodHistoryActivity.java
@@ -1,5 +1,8 @@
 package com.example.rohantaneja.moodtracker.ui;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
@@ -13,12 +16,13 @@ import android.widget.TextView;
 import com.example.rohantaneja.moodtracker.BaseActivity;
 import com.example.rohantaneja.moodtracker.R;
 import com.example.rohantaneja.moodtracker.adapter.MoodHistoryAdapter;
+import com.example.rohantaneja.moodtracker.adapter.receiver.AlarmReceiver;
 import com.example.rohantaneja.moodtracker.model.Mood;
 import com.example.rohantaneja.moodtracker.util.Constants;
 import com.example.rohantaneja.moodtracker.util.SharedPreferenceUtils;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 public class MoodHistoryActivity extends BaseActivity implements View.OnClickListener {
 
@@ -35,6 +39,7 @@ public class MoodHistoryActivity extends BaseActivity implements View.OnClickLis
         setContentView(R.layout.activity_mood_history);
 
         initUI();
+        initMoodHistory();
     }
 
     private void initUI() {
@@ -95,6 +100,24 @@ public class MoodHistoryActivity extends BaseActivity implements View.OnClickLis
         moodsList.add(new Mood(moodId, moodMessage));
 
         return moodsList;
+    }
+
+    private void initMoodHistory() {
+        initAlarmManager();
+    }
+
+    private void initAlarmManager() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+
+        if (alarmManager != null) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmPendingIntent);
+        }
     }
 
 }
