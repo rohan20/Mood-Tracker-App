4a6ccc0e904d2d1540ef614ae09d823e6459dd33
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-09-21 07:28:05 +0530

    added BroadcastReceiver

---
+++ app/src/main/java/com/example/rohantaneja/moodtracker/adapter/receiver/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.example.rohantaneja.moodtracker.adapter.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: 21/09/18 Implement onReceive method
+    }
+}
